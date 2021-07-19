2acb7b75d49da710df2cae142b34c638bd3a38c8
mmaazy <mmaazy18@gmail.com>
2021-07-19 16:09:13 +0500

    Third commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Footer = () =>{
-    return(
-        <>
-    <div className="container-fluid">
-  
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-top pt-5" style={{backgroundColor:"#252584"}}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-1 pt-2">
+              <img src="images/logo.png"/>
+            </div>
+            <div className="col-md-3 pt-4 ml-3">
+              <h3 style={{ color: "white" }}>Easy work</h3>
+            </div>
+            <div className="col-md-3">
+              <h6 style={{ color: "white" }}>Address</h6>
+              <p style={{ color: "white" }}>52-1, Jalan Awan Hijau, Taman, Overseas Union, 58200 Kuala Lumpur, Waliayah Persekutuan Kuala Lumpur</p>
+            </div>
+            <div className="col-md-3">
+              <h6 style={{ color: "white" }}>Contact <p>03-7451 5283</p></h6>
+
+              <h6 style={{ color: "white" }}>Fax <p>03-7451 5283</p></h6>
+            </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,27 +1,51 @@
 import React from "react";
-import { Nav, Navbar, Form, FormControl} from "react-bootstrap";
+import { Nav, Navbar, Form, FormControl } from "react-bootstrap";
 
 const Home = () => {
   return (
     <>
- 
-  <Navbar variant="light">
-  <div className="container">
-    <Navbar.Brand href="#home"><img src="./images/logo.png" alt=""/></Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">About Us</Nav.Link>
-      <Nav.Link href="#pricing">Services</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <Nav.Link href="#pricing">Careers</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    </Form>
-    </div>
-  </Navbar>
-  
+      <div className="container-fluid" style={{ backgroundColor: "#252584" }}>
+        <Navbar variant="light" className="mb-5">
+          <div className="container">
+            <Navbar.Brand href="#home">
+              <img src="./images/logo.png" alt="" />
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home" style={{ color: "white" }}>Home</Nav.Link>
+              <Nav.Link href="#features" style={{ color: "white" }}>About Us</Nav.Link>
+              <Nav.Link href="#pricing" style={{ color: "white" }}>Services</Nav.Link>
+              <Nav.Link href="#pricing" style={{ color: "white" }}>Pricing</Nav.Link>
+              <Nav.Link href="#pricing" style={{ color: "white" }}>Careers</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+            </Form>
+          </div>
+        </Navbar>
 
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mt-5">
+              <div style={{ color: "white" }}>
+                <h1>Make development</h1>
+                <h1>easy with us.</h1>
+              </div>
+
+              <p className="mt-2" style={{ color: "white" }}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores, perspiciatis non. Veniam nulla eaque eos?
+              </p>
+            </div>
+            <div className="col-md-6 mt-5">
+              <img src="images/banner.png" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
